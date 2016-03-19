2c13da1413add971e6abe918f265f2bc3d63235d
Nate Sanchez-Goodwin <Nate@nate-mac.local>
2016-03-19 23:55:39 +1000

    Added some demo content and handlebars setup

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 var express = require('express');
 
 var app = express();
+https = require('https');
 
 app.use(express.static(__dirname + '/public'));
 var exhb = require('express3-handlebars');
 app.engine('handlebars', exhb({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
-
 require('./app/routes.js')(app);
 
 var server = app.listen(3000, function () {
    var host = server.address().address;
    var port = server.address().port;
     
-    console.log('Server listening at http://%s:%s', host, port)
+    console.log('Server listening at http://%s%s', host, port)
 });
--- app/routes.js
+++ app/routes.js
@@ -1,9 +1,25 @@
 module.exports = function (app) {
 
+  var posts = {
+  	"name": "Joshua Holmes",
+  	"profile_picture": "/img/profile_pic.jpg",
+    "cover_picture": "/img/cover_pic.jpg",
+  	"posts": [{
+  		"network": "Twitter",
+  		"title": "How I went from 100 followers to 100 million in just one week.",
+  		"image": "",
+  		"content": ""
+  	}, {
+  		"network": "Twitter",
+  		"title": "Why I started commiting to git more frequently.",
+  		"image": "/img/image_file.jpg",
+  		"content": ""
+  	}]
+  };
     
 app.get('/', function(req, res) {
     data = {};
-    res.render('test', data);
+    res.render('test', posts);
 });
     
 }
