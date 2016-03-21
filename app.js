c083a5d05790e457621fde4ab3e2771c4c4ab88b
Nate Sanchez-Goodwin <Nate@nate-mac.BigPond>
2016-03-21 11:38:10 +1000

    pre presentation code

--- app.js
+++ app.js
@@ -2,17 +2,17 @@ var express = require('express');
 
 var app = express();
 https = require('https');
+var Twitter = require('twitter');
 
 app.use(express.static(__dirname + '/public'));
 var exhb = require('express3-handlebars');
 app.engine('handlebars', exhb({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
+fs = require('fs');
 
-require('./app/routes.js')(app);
-//
-//https.get('https://api.twitter.com/1.1/statuses/user_timeline.json', function(data) {
-//    
-//});
+require('./app/routes.js')(app, Twitter, fs);
+
+
 
 var server = app.listen(3000, function () {
    var host = server.address().address;
--- app/routes.js
+++ app/routes.js
@@ -1,59 +1,109 @@
-module.exports = function (app) {
+module.exports = function (app, Twitter, fs) {
+
+var client = new Twitter({
+  consumer_key: 'KEY_OMITTED',
+  consumer_secret: 'KEY_OMITTED',
+  access_token_key: 'KEY_OMITTED',
+  access_token_secret: 'KEY_OMITTED'
+});
+ 
+function update(posts) {
+    try {
+    fs.writeFile('profile.json', JSON.stringify(posts, null, 2) , 'utf-8');
+    } catch (e) {
+        //handle error
+        console.log(e)
+    }
+}
+
+function getPosts() {
+     var raw = fs.readFileSync("profile.json");   
+     return JSON.parse(raw);
+}
     
-https.get('https://www.behance.net/v2/users/joshdholmes?api_key=LRsUjtF1ZLMB84T7QF7zoS7Mn7jLPNwA', function(res){
-    var body = '';
-
-    res.on('data', function(chunk){
-        body += chunk;
+function tw_followers() {
+    //Get Twitter Follower Count
+    var posts = getPosts();
+    var params = {screen_name: posts.twitter_handle};
+    client.get('followers/ids', params, function(error, data, response){
+      if (!error) {
+        posts.twitter_followers = data.ids.length;
+        update(posts);
+      } else {
+          console.log(error);
+      }
+        
     });
-
-    res.on('end', function(){
-        var response = JSON.parse(body);
-        console.log("Got a response: ", response.picture);
+}
+    
+//tw_followers();
+    
+function ref() {
+    
+    var posts = getPosts();
+    var params = {screen_name: posts.twitter_handle};
+    client.get('statuses/user_timeline', params, function(error, data, response){
+      if (!error) {
+//          console.log(data[0].text);
+//          console.log(data[0].retweeted_status.text);
+// fs.writeFile('dwight.json', JSON.stringify(data, null, 2) , 'utf-8');
+          for(var i = 0;i < data.length;i++) {
+              tweet = {
+                  "url": data[i].user.id,
+                  "tweet": data[i].text,
+                  "user": posts.twitter_handle,
+                  "timestamp": data[i].created_at.substr(0, 10)
+              }
+              
+              try {
+                tweet.image = data[i].entities.media.media_url;
+                console.log(data[i].entities.media.media_url);
+              } catch(e) {
+                  console.log(e);
+              }
+                
+              posts.posts.push(tweet);
+            }
+          update(posts);
+      } else {
+          console.log(error);
+      }
     });
-}).on('error', function(e){
-      console.log("Got an error: ", e);
+       
+}
+    
+    
+function getBehanceData(callbackData){
+  var body = '';
+
+  callback = function(response) {
+      
+        response.on('data', function (chunk) {
+              body += chunk;
+        });
+
+        response.on('end', function () {
+          var response = JSON.parse(body);
+          callbackData(response);
+        });
+  }
+
+  var res = https.get('https://www.behance.net/v2/users/joshdholmes?api_key=KEY_OMITTED',     callback).end();
+
+}
+    
+getBehanceData(function(data){
+    console.log(data);
+    posts.posts[3].title = data.user.username;
+    posts.posts[3].images = data.user.username;
+    posts.posts[3].content = data.user.fields;
 });
 
-  var posts = {
-  	"name": "Joshua Holmes",
-  	"profile_picture": "/img/profile_pic.jpg",
-    "cover_picture": "/img/cover_pic.jpg",
-  	"posts": [{
-  		"twitter": true,
-  		"title": "How I went from 100 followers to 100 million in just one week.",
-  		"image": "",
-  		"content": ""
-  	}, {
-  		"twitter": true,
-  		"title": "Why I started commiting to git more frequently.",
-  		"image": "/img/image_file.jpg",
-  		"content": ""
-  	},
-              {
-  		"github": true,
-  		"title": "Studio39/Composit",
-  		"image": "",
-  		"content": "Creating an easier way to do stuff."
-  	},
-         {
-  		"behance": true,
-  		"title": "A New Type of Imprint Volume Six",
-  		"image": "/img/behance-example.jpg",
-  		"content": "Art Direction • Editorial Design • Photography"
-  	}    ,
-                  {
-  		"bitbucket": true,
-  		"title": "Studio39 Snapchat clone",
-  		"image": "",
-  		"content": "Making snapchat, exactly the same but with less features."
-  	}
-             ]
-  };
+    
     
 app.get('/', function(req, res) {
-    data = {};
-    res.render('feed', posts);
+    var data = getPosts();
+    res.render('feed', data);
 });
     
 }
